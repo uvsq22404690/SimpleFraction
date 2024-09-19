c7e0de5e50ef268a046f59500e3f0ca42072d567
uvsq22404690 <riadnamane@gmail.com>
2024-09-19 23:30:17 +0200

    First commit

---
+++ src/Fraction.java
@@ -0,0 +1,31 @@
+public class Fraction {
+    private double numerator;
+    private double denominator;
+    // Multiplication de fractions
+    // Constructeur
+    public Fraction(double numerateur, double denominateur) {
+        if (denominateur == 0) {
+            throw new IllegalArgumentException("Le dénominateur ne peut pas être zéro.");
+
+        }
+        else {
+            this.numerator = numerateur;
+            this.denominator = denominateur;
+
+        }
+    }
+
+    public  double multiply (double numerateur2, double denominateur2){
+        double fraction1 = this.numerator/this.denominator;
+        double fraction2 = numerateur2/denominateur2;
+        return fraction1*fraction2;
+    }
+
+
+    public double addition(double numerateur2, double denominateur2){
+        double fraction1 = this.numerator/this.denominator;
+        double fraction2 = numerateur2/denominateur2;
+        return fraction1+fraction2;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.io.PrintStream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        PrintStream printf = System.out.printf("Hello and welcome!");
+        // Fraction 1
+        double numerateur1 = 1;
+        double denominateur1 = 0;
+        assert denominateur1 != 0 : "Le dénominateur ne peut pas être zéro.";
+        // Fraction 2
+        double numerateur2 = 5;
+        double denominateur2 = 2;
+        assert denominateur2 != 0 : "Le dénominateur ne peut pas être zéro.";
+
+        Fraction fraction = new Fraction(numerateur1,denominateur1);
+        System.out.printf("La multiplication des deux fractions est : "+fraction.multiply(numerateur2,denominateur2)+"\n");
+        System.out.printf("L'addition des deux fractions est : "+fraction.addition(numerateur2,denominateur2));
+
+
+
+
+
+    }
+}
